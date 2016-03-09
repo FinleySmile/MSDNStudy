6b233299698e64d4c22d7e63d753538f13642dc9
liupanpan <liupanpan@oneapm.com>
2016-03-09 17:20:51 +0800

    Lambda Test Code

---
+++ LambdaTest/LambdaVariableScope/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LambdaVariableScope
+{
+    public delegate bool D();
+
+    public delegate bool D2(int i);
+
+
+    class Program
+    {
+        private D del;
+        private D2 del2;
+
+        public void TestMethod(int input)
+        {
+            int j = 0;
+            del = () =>
+            {
+                j = 10;
+                return j > input;
+            };
+
+            del2 = (x) => x == j;
+
+            Console.WriteLine("before call delegate j = {0}",j);
+
+            bool bResult = del();
+
+            Console.WriteLine("after call del j= {0}, bResult={1}",j,bResult);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.TestMethod(5);
+
+            bool bResult = p.del2(10);
+            Console.WriteLine(bResult);
+
+        }
+    }
+}
---
+++ LambdaTest/Test/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Test
+{
+    public delegate int Calculate(int val) ;
+
+    public delegate bool IsGreater(int x, string s);
+
+    public delegate void Output<T>(T obj);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate cal ;
+            Console.WriteLine("test---");
+            cal = x => x*x;
+
+            Console.WriteLine("test2222---");
+            Console.WriteLine(cal(10));
+
+            IsGreater predicate = (int x, string s) => s.Length > x;
+            
+            Console.WriteLine(predicate(10, "zhangsa"));
+
+            Output<string> output = (str) => { Console.WriteLine(str); };
+            output("aaa");
+
+
+            int[] numbers = { 5, 4, 1, 3, 9, 2, 6, 7, 2, 0 };
+            int oddNumbers = numbers.Count(n => n % 2 == 1);
+            var oddElements = numbers.Where(x => x%2 == 1);
+            Console.WriteLine("odd numbers:{0}",oddNumbers);
+
+            foreach (var element in oddElements)
+            {
+                Console.Write(element + " " );
+            }
+            Console.WriteLine();
+
+            var firstNumbersLessThan6 = numbers.TakeWhile(n => n < 6);
+            foreach (var element in firstNumbersLessThan6)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+
+            var firstSmallNumbers = numbers.TakeWhile((n, index) => {
+                                                                        Console.Write("n:{0},index:{1}",n,index);
+                                                                       return  n > index; });
+            foreach (var element in firstSmallNumbers)
+            {
+                Console.WriteLine(" " + element );
+            }
+            Console.WriteLine();
+        }
+    }
+}
