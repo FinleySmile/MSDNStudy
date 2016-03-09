2ba1b42fbce59110beb702151211fef733da169a
liupanpan <liupanpan@oneapm.com>
2016-03-09 09:32:26 +0800

    Add File IO Test Code

---
+++ FileIOTest/ProcessBarDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ProcessBarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string srcPath = @"D:\aaa";
+            //string destPath = @"E:\aaa";
+            //FileSystem.CopyDirectory(srcPath,destPath);
+
+         //   Write();
+            string aa = "helloworld";
+            var copy = aa.ToLowerInvariant();
+            
+        }
+
+        public static void Write()
+        {
+            string path = @"d:/a.txt";
+            string content = "Hello World";
+            using(var writer = new StreamWriter(path))
+            {
+                writer.WriteLine(content);
+            }
+        }
+    }
+}
