916ef8d7b2428dc226be404928fd85487095929d
liupanpan <liupanpan@oneapm.com>
2016-03-10 22:49:13 +0800

    1.测试匿名类，2.IEnumerable<T> IEnumerable IEnumerator测试

---
+++ HideTypeTest/EnumeratorDemo/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorDemo
+{
+    /// <summary>
+    /// foreach使用，并不要求一定要实现 IEnumerable<T> 或IEnumerable
+    /// 编译器如果没有找到IEnumerable<T> 或IEnumerable，那么就会找GetEnumerator方法
+    /// Duck Tying
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    public class MyList<T>
+    {
+        List<T> _list = new List<T>();
+        private int _currentIndex = 0;
+        public void Add(T element)
+        {
+         _list.Add(element);   
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator<T>(this);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _list[index];
+            }
+            set { _list[index] = value; }
+        }
+
+        public int Count
+        {
+            get { return _list.Count; }
+        }
+        private class Enumerator<T> : IEnumerator<T>
+        {
+            private MyList<T> _myList;
+            private int _currentIndex;
+            public Enumerator(MyList<T> mylist)
+            {
+                _myList = mylist;
+                _currentIndex = 0;
+            }
+            public void Dispose()
+            {
+                Console.WriteLine("释放资源");
+            }
+
+            public bool MoveNext()
+            {
+                return _currentIndex < _myList.Count;
+            }
+
+            public void Reset()
+            {
+                _currentIndex = 0;
+            }
+
+            public T Current 
+            {
+                get
+                {
+                    return _myList[_currentIndex++];
+                } 
+                
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+        }
+    }
+    class Program
+    {
+        public static void TestDuckTyping()
+        {
+            MyList<int> intList = new MyList<int>();
+            for (int i = 1; i < 5; i++)
+            {
+                intList.Add(i);
+            }
+
+            foreach (var element in intList)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stack<int> intStack = new Stack<int>();
+            int[] arr = new[] { 1, 2, 3, 4, 5 };
+            foreach (var i in arr)
+            {
+                intStack.Push(i);
+            }
+
+            Stack<int>.Enumerator enumerator = intStack.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+            TestDuckTyping();
+
+
+        }
+    }
+}
---
+++ HideTypeTest/HideTypeDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideTypeDemo
+{
+    class Program
+    {
+        /// <summary>
+        /// Test方法中MSIL生成生成两个匿名类
+        /// </summary>
+        public static void Test()
+        {
+            var t1 = new { Name = "zhangsan", Year = 21 };
+            var t2 = new { Name = "lisi", Year = 11 };
+            var t3 = new { Name = "wang", BirthYear = t2.Year };
+
+            Console.WriteLine("Name:{0}, Year:{1}", t1.Name, t1.Year);
+            Console.WriteLine("Name:{0}, Year:{1}", t2.Name, t2.Year);
+            Console.WriteLine("Name:{0}, BirthYear:{1}", t1.Name, t3.BirthYear);
+        }
+
+        /// <summary>
+        /// Test方法中MSIL生成生成三个匿名类.
+        ///  两个匿名类要想在同一程序集类型兼容，必须要求属性名，数据类型，属性顺序都完全匹配
+        /// </summary>
+        public static void Test2()
+        {
+            var t1 = new { Name = "zhangsan", Year = 21 };
+            var t2 = new {  Year = 11,Name = "lisi" }; // t1 t2 属性声明顺序不一致，编译器生成MSIL 时，生成两个不同的匿名类
+            var t3 = new { Name = "wang", BirthYear = t2.Year };
+
+            Console.WriteLine("Name:{0}, Year:{1}", t1.Name, t1.Year);
+            Console.WriteLine("Name:{0}, Year:{1}", t2.Name, t2.Year);
+            Console.WriteLine("Name:{0}, BirthYear:{1}", t1.Name, t3.BirthYear);
+        }
+
+
+        static void Main(string[] args)
+        {
+            var t1 = new {Name = "zhangsan", Year = 21 };
+            var t2 = new {Name = "lisi", Year = 11};
+            var t3 = new {Name = "wang", BirthYear = t2.Year};
+
+            Console.WriteLine("Name:{0}, Year:{1}",t1.Name,t1.Year);
+            Console.WriteLine("Name:{0}, Year:{1}", t2.Name, t2.Year);
+            Console.WriteLine("Name:{0}, BirthYear:{1}", t1.Name, t3.BirthYear);
+
+            Console.WriteLine(t3);
+        }
+    }
+}
